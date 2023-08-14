8863d0079c3ce1be00d4e98b0d66b63f2a9649d5
Joy Brar <joybrar2001@gmail.com>
2023-08-15 01:10:37 +0530

    Adding dist

--- src/constants/motion.js
+++ src/constants/motion.js
@@ -1,19 +1,19 @@
-export const fadeIn = (direction, amount, time, delay=0 ) => ({
-    hidden:{
-        x: direction === 'left' ? -amount : direction === 'right' ? amount : 0 ,
-        y: direction === 'up' ? -amount : direction === 'down' ? amount : 0 ,
-        opacity:0,
-        filter: "blur(5px)",
+export const fadeIn = (direction, amount, time, delay = 0) => ({
+  hidden: {
+    x: direction === 'left' ? -amount : direction === 'right' ? amount : 0,
+    y: direction === 'up' ? -amount : direction === 'down' ? amount : 0,
+    opacity: 0,
+    filter: "blur(5px)",
+  },
+  show: {
+    x: 0,
+    y: 0,
+    filter: "blur(0px)",
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: 'easeOut',
+      delay: delay,
     },
-    show:{
-        x: 0,
-        y: 0,
-        filter: "blur(0px)",
-        opacity: 1,
-        transition:{
-            duration: 1,
-            ease: 'easeOut',
-            delay: delay,
-        },
-    },
+  },
 });
