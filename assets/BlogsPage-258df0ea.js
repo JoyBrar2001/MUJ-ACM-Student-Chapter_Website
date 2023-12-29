72e22c6bbec2705d6dd7581b137f5be6354d4405
Joy Brar <joybrar2001@gmail.com>
2023-12-29 23:24:45 +0530

    Adding dist

---
+++ assets/BlogsPage-258df0ea.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-28e0a923.js";const r=()=>s.jsx("div",{children:"Blogs"});export{r as default};
--- assets/BlogsPage-fa40e4e6.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-7352d3eb.js";const r=()=>s.jsx("div",{children:"Blogs"});export{r as default};
