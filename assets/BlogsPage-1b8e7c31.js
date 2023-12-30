2cc3e629253557022933460e6ff2cd9c5e20ed27
Joy Brar <joybrar2001@gmail.com>
2023-12-31 00:42:45 +0530

    Adding dist

--- assets/BlogsPage-1b8e7c31.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as r}from"./index-a2c9d261.js";const t=()=>(o.useEffect(()=>(window.scrollTo(0,0),()=>{window.scrollTo(0,0)}),[]),r.jsx("div",{children:"Blogs"}));export{t as default};
